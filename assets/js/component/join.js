98f7b852152ed89a65531457f49a01e25467c946
Keiner Sanchez <linksanchezmontoya@gmail.com>
2025-08-20 17:56:33 -0500

    te logeaste con exito

--- assets/js/component/join.js
+++ assets/js/component/join.js
@@ -12,3 +12,32 @@ function toggleOtherGender() {
         otherGenderInput.value = '';
     }
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector(".join-form");
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    showToast("Tu solicitud fue enviada con éxito", "success");
+    form.reset();
+  });
+
+  // Función para crear notificaciones
+  function showToast(message, type = "success") {
+    const container = document.getElementById("toast-container");
+    const toast = document.createElement("div");
+    toast.className = `toast ${type}`;
+    toast.innerHTML = `<i class="fa-solid ${type === "success" ? "fa-circle-check" : "fa-circle-xmark"}"></i> ${message}`;
+
+    container.appendChild(toast);
+
+    // Eliminar después de 5s
+    setTimeout(() => {
+      toast.remove();
+    }, 5000);
+
+    // Cerrar manualmente al hacer clic
+    toast.addEventListener("click", () => toast.remove());
+  }
+});
