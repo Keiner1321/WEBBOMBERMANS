953e0bfbe6446f91c93636f581ce49b02dc0476d
Keiner Sanchez <linksanchezmontoya@gmail.com>
2025-10-05 12:00:25 -0500

    depuracion

--- conection/db.php
+++ conection/db.php
@@ -2,12 +2,12 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "bomberosbd"; // Asegúrate de que este sea el nombre correcto
+$dbname = "bomberosbd"; 
 
 // Crear la conexión
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Establecer el charset a utf8mb4 para soportar caracteres especiales (acentos, ñ)
+
 $conn->set_charset("utf8mb4");
 
 // Verificar si la conexión falló y detener la ejecución si es así
